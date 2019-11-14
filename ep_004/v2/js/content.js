c295bdfd0d087db80bb91ba7c6ea96fd03dfab33
Kent Brewster <kentbrew@users.noreply.github.com>
2019-11-14 14:14:07 -0800

    Create content.js

---
+++ ep_004/v2/js/content.js
@@ -0,0 +1,6 @@
+console.log("Content script has loaded via Manifest V2.");
+
+// send a message to the background requesting an additional script
+chrome.runtime.sendMessage({
+  cmd: "runLogic"
+});
