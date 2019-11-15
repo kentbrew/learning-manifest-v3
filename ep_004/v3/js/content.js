43af68bbeeb4b1bb3616fc0bd865edbb4ba92831
Kent Brewster <kentbrew@users.noreply.github.com>
2019-11-15 14:26:38 -0800

    Add console log on start

--- ep_004/v3/js/content.js
+++ ep_004/v3/js/content.js
@@ -1,4 +1,6 @@
+// let us know we're running
 console.log("Content script has loaded via Manifest V3.");
+
 // send a message to the background requesting an additional script
 chrome.runtime.sendMessage({
   cmd: "runLogic"
