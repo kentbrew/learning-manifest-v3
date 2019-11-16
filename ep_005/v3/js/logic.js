d308f6fcbcc92cc341510dd1f5d1f1c3c44b98b3
Kent Brewster <kentbrew@users.noreply.github.com>
2019-11-16 10:50:44 -0800

    Create logic.js

---
+++ ep_005/v3/js/logic.js
@@ -0,0 +1,12 @@
+// let us know we're running
+console.log("A script has run from chrome.tabs.executeScript via Manifest V3.");
+
+// send a message to the background requesting that a context menu be attached
+chrome.runtime.sendMessage({
+  cmd: "addMenu"
+});
+
+// listen for messages from the background
+chrome.runtime.onMessage.addListener(r => {
+  console.log(r);
+});
