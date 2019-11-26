10cc08aa9dd753581673d56dee7f5b5dbb08c68a
Kent Brewster <kentbrew@users.noreply.github.com>
2019-11-26 11:30:17 -0800

    Create background.js

---
+++ ep_007/v3/background.js
@@ -0,0 +1,35 @@
+// listen for a browser session start
+chrome.runtime.onStartup.addListener(() => {
+  // initialize the click counter to zero
+  chrome.storage.local.set({ clickCounter: 0 }, function() {
+    // let us know it worked
+    console.log("V3 Test: initialized test click counter to 0");
+  });
+});
+
+// remove existing menus or else we'll throw an error when we restart
+chrome.contextMenus.removeAll();
+
+// create a menu item
+chrome.contextMenus.create({
+  id: "TestV3",
+  title: "V3 Test",
+  // show the menu over everything
+  contexts: ["all"]
+});
+
+// what to do when our menu is clicked
+chrome.contextMenus.onClicked.addListener(event => {
+  // get the current value
+  chrome.storage.local.get("clickCounter", result => {
+    // bump the count
+    result.clickCounter = result.clickCounter + 1;
+    // update local storage
+    chrome.storage.local.set({ clickCounter: result.clickCounter }, function() {
+      // let us know it worked
+      console.log(
+        "V3 Test: updated test click counter to " + result.clickCounter
+      );
+    });
+  });
+});
