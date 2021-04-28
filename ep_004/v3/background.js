60ee7b961ffe428ad5ca850bd68521a828b95c23
g5fighter <grmapal2@gmail.com>
2021-04-28 11:05:51 +0200

    Inject Script Fixed

--- ep_004/v3/background.js
+++ ep_004/v3/background.js
@@ -1,10 +1,13 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log("Message received!");
   console.log("request: ", request);
   console.log("sender: ", sender);
   if (request.cmd === "runLogic") {
-    chrome.tabs.executeScript(sender.tab.id, {
-      file: "js/logic.js"
-    });
+    chrome.scripting.executeScript(
+      {
+        target: {tabId: sender.tab.id},
+      files: ["js/logic.js"]
+    },
+    () => { });
   }
 });
